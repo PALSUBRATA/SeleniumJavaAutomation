1cf562906336a0606aabeb31954d73e346b23a5a
Subratapal1 <palsubrata746@gmail.com>
2023-11-13 21:18:49 +0530

    2nd

--- Selenium_lab2/src/main/java/com/pal/lab/FirstScript.java
+++ Selenium_lab2/src/main/java/com/pal/lab/FirstScript.java
@@ -1,9 +1,22 @@
 package com.pal.lab;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class FirstScript {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String chromeDriverPath= "C:\\Users\\subra\\git\\SeleniumJavaAutomation\\Selenium_lab2\\chromedriver.exe";
+		String url ="https://chromedriver.chromium.org/downloads";
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get(url);
+		
+		
+		
+		
 
 	}
 
